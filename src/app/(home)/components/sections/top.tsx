25cc8cc4d402dd8a2fbcfa064dd59a91db5df8e7
류정우 <evencoding@gmail.com>
2024-09-19 16:32:38 +0900

    feat: 사각 애니메이션 추가

--- src/app/(home)/components/sections/top.tsx
+++ src/app/(home)/components/sections/top.tsx
@@ -1,29 +1,16 @@
 import ExhibitionHeader from '@/components/exhibition-header'
 import Icon from '@/components/icon'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
+import Title from '../title'
 
 const Top = () => {
-  /* TODO: Fade In Animation */
-  // useGSAP(() => {
-  //   gsap.to('#사각', {})
-  // }, [])
-
   return (
     <section className="pt-[24px] lg:pt-[40px]">
       <ExhibitionHeader />
 
       <div className="flex justify-start md:justify-end">
         <div className="w-full lg:w-[541px]">
-          <div className="mt-[303px] flex w-fit flex-col text-primary-02 md:ml-auto md:mt-[181px] md:items-end lg:ml-0 lg:mt-[164px] lg:items-start">
-            <Icon
-              id="사각"
-              name="사각"
-              className="h-[53px] w-[85px] shrink-0 md:h-[68px] md:w-[109px] lg:h-[161px] lg:w-[258px]"
-            />
-            <div className="text-e-subtitle-01 mt-[9px] leading-[normal] tracking-[auto] md:mt-[13px] lg:mt-mobile lg:text-[28px]">
-              : 사각에 존재하는 것들을 찾아서
-            </div>
+          <div className="mt-[303px] w-fit md:ml-auto md:mt-[181px] lg:mt-[164px]">
+            <Title />
           </div>
 
           <div className="mt-[40px] text-end md:mt-[77px] lg:mt-[325px] lg:text-start">
---
+++ src/app/(home)/components/title.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import Icon from '@/components/icon'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { useRef } from 'react'
+
+const Title = () => {
+  const containerRef = useRef(null)
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline()
+
+      tl.from('#icon', {
+        y: 200,
+        opacity: 0,
+        duration: 0.6,
+        ease: 'power3.out',
+      })
+
+      tl.from(
+        '#text',
+        {
+          y: 80,
+          opacity: 0,
+          duration: 0.6,
+          ease: 'power3.out',
+        },
+        '-=0.4',
+      )
+    },
+    { scope: containerRef },
+  )
+
+  return (
+    <div
+      ref={containerRef}
+      className="flex flex-col text-primary-02 md:ml-auto md:items-end lg:items-start"
+    >
+      <Icon
+        id="icon"
+        name="사각"
+        className="h-[53px] w-[85px] shrink-0 md:h-[68px] md:w-[109px] lg:h-[161px] lg:w-[258px]"
+      />
+      <div
+        id="text"
+        className="text-e-subtitle-01 mt-[9px] leading-[normal] tracking-[auto] md:mt-[13px] lg:mt-mobile lg:text-[28px]"
+      >
+        : 사각에 존재하는 것들을 찾아서
+      </div>
+    </div>
+  )
+}
+
+export default Title
