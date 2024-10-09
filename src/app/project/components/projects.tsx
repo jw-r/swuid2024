f9c66af4fac648c3c647e2c630fe01f320b6fe02
류정우 <evencoding@gmail.com>
2024-10-09 20:45:28 +0900

    qa: project

--- src/app/project/components/projects.tsx
+++ src/app/project/components/projects.tsx
@@ -30,11 +30,11 @@ const Projects = ({ allProjects }: Props) => {
           Project
         </h2>
 
-        <div className="lg:text-web-body-02 mt-[24px] flex gap-[16px] transition-shadow *:w-fit *:hover:hover:shadow-[0_0_15px_0_rgba(254,245,173,0.4)] max-md:flex-col md:mt-[32px] lg:mt-[48px]">
+        <div className="lg:text-web-body-02 text-body-04 mt-[24px] flex gap-[16px] transition-shadow *:w-fit *:hover:hover:shadow-[0_0_15px_0_rgba(254,245,173,0.4)] max-md:flex-col md:mt-[32px] lg:mt-[48px]">
           <button
             onClick={() => setSort('UX')}
             className={cn(
-              'border border-primary-02 bg-primary-02/20 px-[36px] py-[13px] opacity-30 hover:opacity-100 transition-all duration-300 ease-out',
+              'border border-primary-02 bg-primary-02/20 px-[16px] py-[10px] lg:px-[36px] lg:py-[13px] opacity-30 hover:opacity-100 transition-all duration-300 ease-out',
               sort === 'UX' && 'opacity-100',
             )}
           >
@@ -43,7 +43,7 @@ const Projects = ({ allProjects }: Props) => {
           <button
             onClick={() => setSort('DF')}
             className={cn(
-              'border border-primary-02 bg-primary-02/20 px-[36px] py-[13px] opacity-30 hover:opacity-100 transition-all duration-300 ease-out',
+              'border border-primary-02 bg-primary-02/20 px-[16px] py-[10px] lg:px-[36px] lg:py-[13px] opacity-30 hover:opacity-100 transition-all duration-300 ease-out',
               sort === 'DF' && 'opacity-100',
             )}
           >
@@ -52,7 +52,7 @@ const Projects = ({ allProjects }: Props) => {
           <button
             onClick={() => setSort('BX')}
             className={cn(
-              'border border-primary-02 bg-primary-02/20 px-[36px] py-[13px] opacity-30 hover:opacity-100 transition-all duration-300 ease-out',
+              'border border-primary-02 bg-primary-02/20 px-[16px] py-[10px] lg:px-[36px] lg:py-[13px] opacity-30 hover:opacity-100 transition-all duration-300 ease-out',
               sort === 'BX' && 'opacity-100',
             )}
           >
@@ -81,8 +81,10 @@ const Projects = ({ allProjects }: Props) => {
                 </div>
               </div>
               <div className="mt-mobile lg:hidden">
-                <div className="text-subtitle-01">프로젝트 타이틀</div>
-                <div className="text-body-02 mt-[5px]">팀원1 팀원2 팀원3 팀원4</div>
+                <div className="text-subtitle-01">{project.name}</div>
+                <div className="text-body-02 mt-[5px]">
+                  {project.designers.map((designer) => designer.name).join(' ')}
+                </div>
               </div>
             </Link>
           ))}
