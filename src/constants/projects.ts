b3f2f1499295cf71c4776692e5dab5987a06696e
류정우 <evencoding@gmail.com>
2024-10-27 13:55:59 +0900

    feat: projects, designers 상수화

---
+++ src/constants/projects.ts
@@ -0,0 +1,319 @@
+export const projects = {
+  BX: [
+    {
+      id: 1,
+      members: [
+        { name: '김유진', classNumber: '2020111425' },
+        { name: '정예승', classNumber: '2020111526' },
+        { name: '허찬비', classNumber: '2019111457' },
+      ],
+      title: '유어예(遊於藝)',
+      teamName: null,
+    },
+    {
+      id: 2,
+      members: [
+        { name: '김채원', classNumber: '2020111427' },
+        { name: '이지은', classNumber: '2020111447' },
+      ],
+      title: '희피당(喜被堂)',
+      teamName: null,
+    },
+    {
+      id: 3,
+      members: [
+        { name: '박인애', classNumber: '2020111436' },
+        { name: '정다은', classNumber: '2021111449' },
+      ],
+      title: '넷필리온',
+      teamName: null,
+    },
+    {
+      id: 4,
+      members: [
+        { name: '이수현', classNumber: '2018110956' },
+        { name: '오유진', classNumber: '2020111440' },
+      ],
+      title: 'GG 3.5',
+      teamName: null,
+    },
+  ],
+
+  DF: [
+    {
+      id: 1,
+      members: [{ name: '강지은', classNumber: '2020111419' }],
+      title: '강지은졸전',
+      teamName: null,
+    },
+    {
+      id: 2,
+      members: [{ name: '고경희', classNumber: '2020111421' }],
+      title: 'IOT TIMER',
+      teamName: null,
+    },
+    {
+      id: 3,
+      members: [{ name: '고은지', classNumber: '2020111423' }],
+      title: '고은지졸전',
+      teamName: null,
+    },
+    {
+      id: 4,
+      members: [{ name: '김소은', classNumber: '2019111419' }],
+      title: '김소은419졸전',
+      teamName: null,
+    },
+    {
+      id: 5,
+      members: [{ name: '김소은', classNumber: '2020111505' }],
+      title: '김소은505졸전',
+      teamName: null,
+    },
+    {
+      id: 6,
+      members: [{ name: '김지현', classNumber: '2021111430' }],
+      title: '김지현졸전',
+      teamName: null,
+    },
+    {
+      id: 7,
+      members: [{ name: '문가희', classNumber: '2020111431' }],
+      title: '문가희졸전',
+      teamName: null,
+    },
+    {
+      id: 8,
+      members: [{ name: '박수연', classNumber: '2020111434' }],
+      title: '리프트 타임(Lift Time)',
+      teamName: null,
+    },
+    {
+      id: 9,
+      members: [{ name: '박은아', classNumber: '2020111435' }],
+      title: '계량 용기',
+      teamName: null,
+    },
+    {
+      id: 10,
+      members: [{ name: '박이은', classNumber: '2021120105' }],
+      title: 'Sad Ending ㅠㅅㅠ',
+      teamName: null,
+    },
+    {
+      id: 11,
+      members: [{ name: '반유진', classNumber: '2021111438' }],
+      title: '레드포인트(Redpoint)',
+      teamName: null,
+    },
+    {
+      id: 12,
+      members: [{ name: '우규리', classNumber: '2020111442' }],
+      title: '우규리졸전',
+      teamName: null,
+    },
+    {
+      id: 13,
+      members: [{ name: '유주하', classNumber: '2021111443' }],
+      title: 'open tag',
+      teamName: null,
+    },
+    {
+      id: 14,
+      members: [{ name: '윤주하', classNumber: '2018111439' }],
+      title: '윤주하졸전',
+      teamName: null,
+    },
+    {
+      id: 15,
+      members: [{ name: '이가연', classNumber: '2020111444' }],
+      title: 'Football',
+      teamName: null,
+    },
+    {
+      id: 16,
+      members: [{ name: '이서영', classNumber: '2020111445' }],
+      title: '이서영졸전',
+      teamName: null,
+    },
+    {
+      id: 17,
+      members: [{ name: '이소연', classNumber: '2020111446' }],
+      title: '캐리어넴택',
+      teamName: null,
+    },
+    {
+      id: 18,
+      members: [{ name: '이지은', classNumber: '2020111448' }],
+      title: '이지은448졸전',
+      teamName: null,
+    },
+    {
+      id: 19,
+      members: [{ name: '장윤서', classNumber: '2021111447' }],
+      title: 'MirrorEase',
+      teamName: null,
+    },
+    {
+      id: 20,
+      members: [{ name: '전예지', classNumber: '2021120106' }],
+      title: 'HOME',
+      teamName: null,
+    },
+    {
+      id: 21,
+      members: [{ name: '정가윤', classNumber: '2021111448' }],
+      title: '정가윤졸전',
+      teamName: null,
+    },
+    {
+      id: 22,
+      members: [{ name: '조수민', classNumber: '2020111454' }],
+      title: '제품 디자인',
+      teamName: null,
+    },
+    {
+      id: 23,
+      members: [{ name: '조수빈', classNumber: '2020111455' }],
+      title: '조수빈졸전',
+      teamName: null,
+    },
+    {
+      id: 24,
+      members: [{ name: '지예성', classNumber: '2021111450' }],
+      title: '강아지강아지',
+      teamName: null,
+    },
+    {
+      id: 25,
+      members: [{ name: '홍수정', classNumber: '2020111458' }],
+      title: 'ㅇㅇ',
+      teamName: null,
+    },
+    {
+      id: 26,
+      members: [{ name: '황예린', classNumber: '2021111456' }],
+      title: '우산',
+      teamName: null,
+    },
+  ],
+
+  UX: [
+    {
+      id: 1,
+      members: [
+        { name: '김지현', classNumber: '2021111430' },
+        { name: '박인애', classNumber: '2020111436' },
+        { name: '전예지', classNumber: '2021120106' },
+        { name: '황예린', classNumber: '2021111456' },
+      ],
+      title: 'SOSOM',
+      teamName: '2월의 춘',
+    },
+    {
+      id: 2,
+      members: [
+        { name: '이가연', classNumber: '2020111444' },
+        { name: '이소연', classNumber: '2020111446' },
+        { name: '홍수정', classNumber: '2020111458' },
+      ],
+      title: '모멘타임(Momentime)',
+      teamName: '공릉동 특공대',
+    },
+    {
+      id: 3,
+      members: [
+        { name: '반유진', classNumber: '2021111438' },
+        { name: '정가윤', classNumber: '2021111448' },
+        { name: '정다은', classNumber: '2021111449' },
+        { name: '천유정', classNumber: '2019111455' },
+      ],
+      title: '마디(MADI)',
+      teamName: '달큰걸즈',
+    },
+    {
+      id: 4,
+      members: [
+        { name: '신주은', classNumber: '2020111438' },
+        { name: '이혜인', classNumber: '2020111449' },
+      ],
+      title: 'Très Bion (트리비온)',
+      teamName: '듀얼크루',
+    },
+    {
+      id: 5,
+      members: [
+        { name: '고경희', classNumber: '2020111421' },
+        { name: '고은지', classNumber: '2020111423' },
+        { name: '김유진', classNumber: '2020111425' },
+        { name: '박은아', classNumber: '2020111435' },
+      ],
+      title: 'Storeet(스토릿)',
+      teamName: '막둥이',
+    },
+    {
+      id: 6,
+      members: [
+        { name: '김채원', classNumber: '2020111427' },
+        { name: '우규리', classNumber: '2020111442' },
+        { name: '이서영', classNumber: '2020111445' },
+        { name: '조수빈', classNumber: '2020111455' },
+      ],
+      title: '커밋티',
+      teamName: '세컨아워',
+    },
+    {
+      id: 7,
+      members: [
+        { name: '박수연', classNumber: '2020111434' },
+        { name: '유주하', classNumber: '2021111443' },
+        { name: '조수민', classNumber: '2020111454' },
+        { name: '황자경', classNumber: '2020111460' },
+      ],
+      title: 'SWAY(스웨이)',
+      teamName: '쉼표',
+    },
+    {
+      id: 8,
+      members: [
+        { name: '강지은', classNumber: '2020111419' },
+        { name: '윤주하', classNumber: '2018111439' },
+        { name: '오유진', classNumber: '2020111440' },
+        { name: '정예승', classNumber: '2020111526' },
+      ],
+      title: '모아(MOAA)',
+      teamName: '스물다섯 스물셋',
+    },
+    {
+      id: 9,
+      members: [
+        { name: '김소은', classNumber: '2019111419' },
+        { name: '박이은', classNumber: '2021120105' },
+        { name: '이지은', classNumber: '2020111447' },
+        { name: '이지은', classNumber: '2020111448' },
+      ],
+      title: 'LOCALINK',
+      teamName: '은포',
+    },
+    {
+      id: 10,
+      members: [
+        { name: '김소은', classNumber: '2020111505' },
+        { name: '문가희', classNumber: '2020111431' },
+      ],
+      title: '퍼플(Purple)',
+      teamName: '이인삼각',
+    },
+    {
+      id: 11,
+      members: [
+        { name: '공성연', classNumber: '2020111233' },
+        { name: '송의진', classNumber: '2019111266' },
+        { name: '지예성', classNumber: '2021111450' },
+        { name: '장윤서', classNumber: '2021111447' },
+      ],
+      title: '바톤췤!',
+      teamName: '바톤땃쥐',
+    },
+  ],
+} as const
