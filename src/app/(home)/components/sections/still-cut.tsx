94fb10af6102dd7008ed6d626dee73f43c7b62b8
류정우 <evencoding@gmail.com>
2024-10-29 13:00:54 +0900

    feat: steal cuts link

--- src/app/(home)/components/sections/still-cut.tsx
+++ src/app/(home)/components/sections/still-cut.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Section from '../section'
+import Link from 'next/link'
 
 const Interview = () => {
   const stillCuts = ['/steal/cut01.webp', '/steal/cut02.webp', '/steal/cut03.webp']
@@ -8,12 +9,14 @@ const Interview = () => {
     <Section title="스틸컷" className="mt-[135px] md:mt-[135px] lg:mt-[366px]">
       <div className="mt-[10px] flex gap-[16px] overflow-x-scroll md:mt-[20px] lg:mt-[36px]">
         {stillCuts.map((src) => (
-          <div
+          <Link
             key={src}
+            target="_blank"
+            href="https://www.instagram.com/swu_id/"
             className="relative h-[154.5px] min-w-[230px] cursor-pointer border border-primary-02/70 md:h-[201.2px] md:min-w-[300px] lg:h-[360px] lg:min-w-0 lg:flex-1"
           >
             <Image src={src} alt="" fill className="object-cover" />
-          </div>
+          </Link>
         ))}
       </div>
     </Section>
